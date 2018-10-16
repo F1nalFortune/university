d89618cd67a5500eef0b2cd10ee79c8dabd44784
Brian Phelps <phelpsb92@gmail.com>
2018-10-15 23:26:41 -0400

    auth

--- home.php
+++ home.php
@@ -19,6 +19,18 @@ $password = "";
 $dbname = "university";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+$sql="SELECT * FROM users WHERE users.username={$_POST['username']} AND users.password={$_POST['password']}";
+$results = $conn->query($sql);
+if ($results->num_rows > 0){
+  while($data = $results->fetch_assoc()){
+    $_SESSION['id']=$data['id'];
+    $_SESSION['username'] = $data['username'];
+    $_SESSION['success'] = "You are now logged in!";
+  }
+}else{
+  echo "error retrieving user";
+}
+
 
 if($_SESSION['username'] != null){
   echo "Welcome, ".$_SESSION["username"];
